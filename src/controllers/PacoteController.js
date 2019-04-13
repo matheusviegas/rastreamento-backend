eadd85d81c9caafbe03b446ff55fbc5f1069b892
Matheus Souza <matheus@mertins.com.br>
2019-04-13 16:44:54 -0300

    Modificando para retornar apenas um pacote e criando metodo para listar todos os pacotes

--- src/controllers/PacoteController.js
+++ src/controllers/PacoteController.js
@@ -9,15 +9,23 @@ class PacoteController {
     return res.json(pacote);
   }
 
+  async listar(req, res) {
+    const pacotes = await Pacote.find();
+
+    return res.json(pacotes);
+  }
+
   async buscar(req, res) {
-    const pacote = await Pacote.find({ codigo: req.params.codigo }).populate({
-      path: "atualizacoes",
-      options: {
-        sort: {
-          createdAt: 1
+    const pacote = await Pacote.findOne({ codigo: req.params.codigo }).populate(
+      {
+        path: "atualizacoes",
+        options: {
+          sort: {
+            createdAt: 1
+          }
         }
       }
-    });
+    );
 
     return res.json(pacote);
   }
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const routes = express.Router();
 const PacoteController = require("./controllers/PacoteController");
 const AtualizacaoController = require("./controllers/AtualizacaoController");
 
+routes.get('/pacote', PacoteController.listar);
 routes.post("/pacote", PacoteController.salvar);
 routes.post("/pacote/:codigo/atualizacao", AtualizacaoController.salvar);
 routes.get("/pacote/:codigo", PacoteController.buscar);
